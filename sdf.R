511c07ef503fd6acb06bc1f8e6b23106839f05d1
Derek Holste <dahol@DESKTOP-SJVUHBN.localdomain>
2021-04-14 11:25:04 -0700

    1

---
+++ sdf.R
@@ -0,0 +1,21 @@
+##Code for my Projects######
+library(usethis)
+library(gitcreds)
+
+use_git(message = "First Presentation")
+
+#Checking for any pre-existing remotes
+git_remotes()
+
+##If you have a pre-existing origin
+#use_git_remote(name = "origin", url, overwrite = FALSE)
+
+
+#Connects a local repo with GitHub
+use_github(
+  organisation = NULL,
+  private = FALSE,
+  protocol = git_protocol(),
+  host = NULL
+)
+dcf
